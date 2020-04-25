a6e30d8ec693940d83cec313b6045e6102aa2955
statiolake <statiolake@gmail.com>
2020-04-25 23:39:57 +0900

    Add doc for define_modint_const!

--- src/pcl/math/modint/consts_2016.rs
+++ src/pcl/math/modint/consts_2016.rs
@@ -5,6 +5,26 @@ pub trait ModintConst {
     fn get_modulus() -> ModintInnerType;
 }
 
+/// `Modint` の定数 (`ModintConst` を実装する型) を簡単に定義するためのマクロ。
+///
+/// # Examples
+///
+/// ```
+/// # #[cfg(feature = "rust2016")]
+/// # #[macro_use] extern crate procon_lib;
+/// # #[cfg(feature = "rust2020")]
+/// # use procon_lib::define_modint_const;
+/// // use crate::define_modint_const;
+/// define_modint_const! {
+///     pub const MOD11 = 11;
+/// }
+///
+/// # use procon_lib::pcl::math::Modint;
+/// // use pcl::math::Modint;
+/// type M11 = Modint<MOD11>;
+/// #
+/// # fn main() {}
+/// ```
 // polyfill: use function instead
 #[macro_export]
 macro_rules! define_modint_const {
--- src/pcl/math/modint/consts_2020.rs
+++ src/pcl/math/modint/consts_2020.rs
@@ -4,6 +4,26 @@ pub trait ModintConst {
     const MOD: ModintInnerType;
 }
 
+/// `Modint` の定数 (`ModintConst` を実装する型) を簡単に定義するためのマクロ。
+///
+/// # Examples
+///
+/// ```
+/// # #[cfg(feature = "rust2016")]
+/// # #[macro_use] extern crate procon_lib;
+/// # #[cfg(feature = "rust2020")]
+/// # use procon_lib::define_modint_const;
+/// // use crate::define_modint_const;
+/// define_modint_const! {
+///     pub const MOD11 = 11;
+/// }
+///
+/// # use procon_lib::pcl::math::Modint;
+/// // use pcl::math::Modint;
+/// type M11 = Modint<MOD11>;
+/// #
+/// # fn main() {}
+/// ```
 #[macro_export]
 macro_rules! define_modint_const {
     ($(#[doc = $doc:expr])* pub const $name:ident = $value:literal;) => {
