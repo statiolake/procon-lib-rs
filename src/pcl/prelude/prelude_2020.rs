87806772e3de7bc961b6fa594eec3684d82497f4
statiolake <statiolake@gmail.com>
2020-05-24 02:54:20 +0900

    Add maplit to prelude

--- src/pcl/prelude/prelude_2020.rs
+++ src/pcl/prelude/prelude_2020.rs
@@ -1,4 +1,5 @@
 pub use itertools::Itertools;
+pub use maplit::{btreemap, btreeset, convert_args, hashmap, hashset};
 pub use proconio::fastout;
 pub use proconio::input;
 pub use proconio::marker::*;
